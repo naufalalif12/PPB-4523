fd91615cf47ed7a4eef47dbb71ef1622d9a1dd68
naufalalif12 <naufalalif06803@gmail.com>
2021-10-20 00:17:21 +0700

    update tugas

--- Materi5/app/src/main/java/com/example/materi5/MainActivity.java
+++ Materi5/app/src/main/java/com/example/materi5/MainActivity.java
@@ -1,26 +1,88 @@
 package com.example.materi5;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    ListView main;
-    public String bhs_prog[]={"Prolog", "C", "C++", "Python", "Java", "Kotlin", "Delphi"};
-
-    Spinner comboitem;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
+    Button btnAdd;
+    EditText inputLabel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spinner);
+        btnAdd =  findViewById(R.id.btn_add);
+        inputLabel = findViewById(R.id.input_label);
 
-        main = (ListView) findViewById(R.id.list_item);
-        comboitem = (Spinner) findViewById(R.id.comboitem);
+        spinner.setOnItemSelectedListener(this);
 
-        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, bhs_prog);
-        comboitem.setAdapter(adapter);
+        loadSpinnerData();
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View arg0) {
+                String label = inputLabel.getText().toString();
+
+                if (label.trim().length() > 0) {
+                    DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+                    db.insertLabel(label);
+
+                    inputLabel.setText("");
+
+                    InputMethodManager imm = (InputMethodManager)
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(inputLabel.getWindowToken(), 0);
+                    loadSpinnerData();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter label name",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+
+    private void loadSpinnerData() {
+        DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+        List<String> labels = db.getAllLabels();
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, labels);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+                               long id) {
+        String label = parent.getItemAtPosition(position).toString();
+
+        Toast.makeText(parent.getContext(), "You selected: " + label,
+                Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
     }
 }
